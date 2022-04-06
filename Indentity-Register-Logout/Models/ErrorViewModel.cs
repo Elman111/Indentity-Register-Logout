ad122e8159a02f6c9614456d92b562a44333ef4f
Elman111 <elmannn@code.edu.az>
2022-04-06 08:42:21 +0400

    Indentity-Register-Logout

---
+++ Indentity-Register-Logout/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Indentity_Register_Logout.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
